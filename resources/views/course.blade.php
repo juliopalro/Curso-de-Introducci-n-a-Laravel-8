8d6c16bbf85b0a92fd9d7861eebf04313c75bc64
juliopalro <juliopalro@gmail.com>
2021-04-13 21:11:06 -0500

    Cursos similares

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -26,10 +26,23 @@
                     <p class="text-gray-300 text-xs">{{ $course->created_at->diffForHumans() }}</p>
                 </div>
             </div>
+            <div class="grid grid-cols-2 gap-4 my-8">
+               @foreach ($course->similary() as $course)
+               <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+                    <a href="{{ route('course', $course->slug) }}">
+                        <img src="{{ $course->image }}" class="rounded-md mb-2">
+                        <h2 class="text-lg text-gray-600 truncate uppercase">{{ $course->name }}</h2>
+                        <h3 class="text-md text-gray-500">{{ $course->excerpt }}</h3>
+
+                        <img src="{{ $course->user->avatar }}" class="rounded-full mt-4 mx-auto h-16 w-16">
+                    </a>
+                </div>
+                @endforeach
+            </div>
         </div>
     </div>
 
-    <div class="text-center">
+    <div class="text-center mt-4">
         <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos cursos</h1>
         <h2 class="text-xl text-gray-600">Fórmate online como profesional en tecnología</h2>
     </div>
